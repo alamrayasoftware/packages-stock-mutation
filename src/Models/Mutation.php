b01d4460ff8069fc1e0ca3f8019457d6d05793e4
aidensV <nameblacllis@gmail.com>
2021-06-01 13:24:48 +0700

    delete name column

--- src/Models/Mutation.php
+++ src/Models/Mutation.php
@@ -5,9 +5,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mutation extends Model
 {
-    protected $table = 'stock_mutation';
-    protected $primaryKey = 'id';
-
     public function stock()
     {
         return $this->belongsTo(Stock::class,'stock_id','id');
--- src/Models/Stock.php
+++ src/Models/Stock.php
@@ -5,6 +5,4 @@ use Illuminate\Database\Eloquent\Model;
 
 class Stock extends Model
 {
-    protected $table = 'stock';
-    protected $primaryKey = 'id';
 }
