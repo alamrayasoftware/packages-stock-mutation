e75c8f93dcaf2710892f8bb2c486a3e13349650f
Rizqon <m.rum.rowi.r@gmail.com>
2021-06-03 15:30:11 +0700

    modified migration publish

--- src/StockMutationServiceProvider.php
+++ src/StockMutationServiceProvider.php
@@ -14,7 +14,11 @@ class StockMutationServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        $this->publishes([
+            __DIR__ . '/../database/migrations/' => database_path('migrations')
+        ], 'migrations');
     }
 
     /**
